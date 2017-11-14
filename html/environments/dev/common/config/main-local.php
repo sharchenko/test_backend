644473f0eca1fc2e54be82b26efd6b3c6e18be8a
vrasenko@onesolv.com <AsjRpEhUKuCf>
2017-11-14 22:22:29 +0200

    Адаптировал шаблон * удалил vagrant конфиг * удалил backend * frontend переименовал в app

--- html/environments/dev/common/config/main-local.php
+++ html/environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'dsn' => 'pgsql:host=database;dbname=main_base',
             'username' => 'root',
-            'password' => '',
+            'password' => 'root',
             'charset' => 'utf8',
         ],
         'mailer' => [
